106b1c17984cc042da6a374d7905435303427244
emanelsaeid7-collab <eman.elsaeid7@gmail.com>
2026-06-08 05:31:05 +0300

    Update Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -12,7 +12,23 @@ export const Root = () => {
       width={1080}
       height={1920}
       durationInFrames={900}
-      defaultProps={{ videoData: {} }}
+      calculateMetadata={async ({ props }) => {
+        const data = props?.videoData || {};
+        const ctaFrames = data.ctaDurFrames || 150;
+
+        if (data.sceneTimings && data.sceneTimings.length > 0) {
+          const lastTiming = data.sceneTimings[data.sceneTimings.length - 1];
+          const scenesDurationFrames = Math.ceil(lastTiming.end * FPS);
+
+          console.log(`[metadata] scenes=${scenesDurationFrames} + cta=${ctaFrames} = ${scenesDurationFrames + ctaFrames}`);
+
+          return {
+            durationInFrames: scenesDurationFrames + ctaFrames,
+          };
+        }
+
+        return { durationInFrames: 900 };
+      }}
     />
   );
 };
